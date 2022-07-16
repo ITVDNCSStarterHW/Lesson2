764b2786bd59a20cc84512c47da9212ccf94a32c
Никита Бочаров <bochanikita@yandex.ru>
2022-07-16 13:04:33 +0300

    Добавьте файлы проекта.

---
+++ Less1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Less1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.141592653d;
+            const double e = 2.7182818284d;
+            Console.WriteLine("pi = {0}, e = {1}", pi, e);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nmy string 1");
+            Console.WriteLine("\tmy string 2");
+            Console.WriteLine("\amy string 3");
+            Console.ReadLine();
+        }
+    }
+}
